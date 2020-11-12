866732db21adbb867daad6a063076bfb4672e2d2
pem725 <pem725@gmail.com>
2020-11-12 11:29:07 -0500

    added S&P 500 data to the forecasting model

--- tryoutcodehere.R
+++ tryoutcodehere.R
@@ -41,3 +41,64 @@ for (i in 1:nrow(tryme)){
 
 
 tryme
+
+### webscrape the S&P500 data:
+library(rvest)
+
+url <- 'https://en.wikipedia.org/wiki/S%26P_500_Index'
+url2 <- html('https://en.wikipedia.org/wiki/S%26P_500_Index')
+
+## note, I got the xpath by copying from the wikipedia page and using F12 to find it
+sp500.d <- url %>%
+  read_html() %>%
+  html_nodes(xpath='//*[@id="mw-content-text"]/div[1]/div[8]/table') %>%
+  html_table()
+sp500.d <- sp500.d[[1]]
+
+library(psych)
+describe(sp500.d)
+str(sp500.d)
+sp500.d$Yr <- as.numeric(sp500.d$Year)
+table(sp500.d$Yr)
+sp500.d$YTDret <- sp500.d$`Change in Index`
+#sp500.d$YTDret <- as.numeric(sp500.d$`Change in Index`)
+#sp500.d$YTDret = as.numeric(gsub("[\\%,]", "", sp500.d$`Change in Index`))
+
+
+library(dplyr)
+library(readr)
+
+d <- sp500.d[-54,10:11]
+res <- cbind(d %>% select(Yr),
+             d %>% select(-Yr) %>% mutate_all(funs(parse_number)))
+describe(res)
+res$YTDret
+Negivs <- as.numeric(substr(d$YTDret,1,1))
+res$YTDret[is.na(Negivs)] <- res$YTDret[is.na(Negivs)]*-1
+res$YTDret
+mean(res$YTDret)
+sd(res$YTDret)
+rnorm(1,mean(res$YTDret),sd(res$YTDret))
+hist(res$YTDret)
+
+YTDret.ts <- ts(res$YTDret[1:50],1970,2019)
+YTDret.ts
+plot.ts(YTDret.ts)
+library(TTR)
+
+YTDret.sma <- SMA(YTDret.ts,6)
+plot(YTDret.sma)
+
+YTDret.decomp <- decompose(YTDret.ts)
+YTDret.forecast <- HoltWinters(YTDret.ts, beta=F, gamma=F)
+plot(YTDret.forecast)
+
+library(forecast)
+YTDret.forecast2 <- forecast(YTDret.forecast, h=30)
+YTDret.forecast2
+
+
+
+library(ggplot2)
+ggplot(res,aes(x=Yr,y=YTDret)) + geom_smooth()
+
